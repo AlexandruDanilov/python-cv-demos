fc8416bd9fd4dfeb79de18e64b8577c9db49cbde
Dani <87378814+AlexandruDanilov@users.noreply.github.com>
2024-02-02 10:57:11 +0200

    Create color_detection_pi3_buster_hog_descriptor.py
    raspberry pi 3 model b running raspberrypi os Buster
    standard cv2 camera color detection
    based on hsv color code
    developed for Chembot 2022

---
+++ Raspberry pi model 3b Buster/color_detection_pi3_buster_hog_descriptor.py
@@ -0,0 +1,63 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import numpy as np
+import cv2
+
+def get_color_threshold(color):
+    if color == 'blue':
+        return (90, 80, 0), (120, 255, 255)
+    elif color == 'green':
+        return (35, 80, 0), (85, 255, 255)
+    elif color == 'white':
+        return (0, 0, 200), (255, 30, 255)
+    else:
+        raise ValueError('Invalid color')
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 10
+camera.rotation = 0
+
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+time.sleep(0.1)
+
+color = 'blue'
+lower, upper = get_color_threshold(color)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+    image = frame.array
+    cv2.line(image, (213, 0), (213, 480), (204, 0, 204), 2)
+    cv2.line(image, (426, 0), (426, 480), (204, 0, 204), 2)
+    
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, lower, upper)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 100:  # Filter out small areas to remove noise
+            x, y, w, h = cv2.boundingRect(contour)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 51, 51), 2)
+            cv2.line(image, (round((x + x + w) / 2), 0), (round((x + x + w) / 2), 480), (0, 153, 51), 2)
+            if round((x + x + w) / 2) <= 213:
+                print("1")
+            if 213 < round((x + x + w) / 2) < 426:
+                print("2")
+            if round((x + x + w) / 2) >= 426:
+                print("3")
+
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+    
+    if key == ord("q"):
+        break
+    elif key == ord("g"):
+        color = 'green'
+        lower, upper = get_color_threshold(color)
+
+    rawCapture.truncate(0)
+
+cv2.destroyAllWindows()
